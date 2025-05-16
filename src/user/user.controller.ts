a9aa8c7047d6b66c281d568bdfd5a758997a836a
mtoanp <mtoanp@outlook.com>
2025-05-16 13:20:33 +0200

    feat(user): complete user-module with mock data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,37 +10,40 @@ import {
   ParseIntPipe,
   ValidationPipe,
 } from '@nestjs/common';
-
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('users')
+@Controller('users') // /users
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  // Dependency Injection
+  constructor(private readonly usersService: UserService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  @Get() // GET /users or /users?role=value
+  findAll(@Query('role') role?: 'user' | 'admin') {
+    return this.usersService.findAll(role);
   }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
+  @Get(':id') // GET /:id
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  @Post() // POST /
+  create(@Body(ValidationPipe) createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+  @Patch(':id') // PATCH /:id
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe) updateUserDto: UpdateUserDto,
+  ) {
+    return this.usersService.update(id, updateUserDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @Delete(':id') // DELETE /:id
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.remove(id);
   }
 }
