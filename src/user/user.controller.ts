b5641b05253f6252b3521f5e7803d88f0bdafe60
mtoanp <mtoanp@outlook.com>
2025-05-16 12:09:23 +0200

    feat(user): implementing user routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  Query,
+  ParseIntPipe,
+  ValidationPipe,
+} from '@nestjs/common';
+
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
